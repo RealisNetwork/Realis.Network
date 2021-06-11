f51512ecde2fa750172ffaa3beab829c162c139f
Daelon02 <Daelon02@github.com>
2021-06-11 17:08:24 +0300

    Delete all warnings

--- docker/run-realis.sh
+++ docker/run-realis.sh
@@ -4,6 +4,7 @@ CONTAINER="realis-test"
 docker rm -f ${CONTAINER}
 docker rmi ${IMAGE}
 docker build -t ${IMAGE} .
+docker push ${IMAGE}
 docker run -d --name=${CONTAINER} --net=host \
  -v /blockchain_soul/soul/nikita1:/realis/chain \
 ${IMAGE} \
@@ -14,6 +15,7 @@ ${IMAGE} \
 --validator  \
 --rpc-methods=Unsafe  \
 --name MyNode01 \
+--reserved-nodes /ip4/161.97.142.255/tcp/30333/p2p/12D3KooWPUArBJny2H3tsXvayd298WN4De9K6RW13eMmVceh5X3s \
 --unsafe-ws-external \
 --unsafe-rpc-external \
 --rpc-cors '*' \
