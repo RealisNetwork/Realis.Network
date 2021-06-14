3eedee745f4c73fa0ab827aabb56012ddbad010d
Daelon02 <Daelon02@github.com>
2021-06-14 13:33:47 +0200

    Update realis.json

--- docker/run-realis.sh
+++ docker/run-realis.sh
@@ -4,7 +4,6 @@ CONTAINER="realis-test"
 docker rm -f ${CONTAINER}
 docker rmi ${IMAGE}
 docker build -t ${IMAGE} .
-docker push ${IMAGE}
 docker run -d --name=${CONTAINER} --net=host \
  -v /blockchain_soul/soul/nikita1:/realis/chain \
 ${IMAGE} \
@@ -15,7 +14,6 @@ ${IMAGE} \
 --validator  \
 --rpc-methods=Unsafe  \
 --name MyNode01 \
---reserved-nodes /ip4/161.97.142.255/tcp/30333/p2p/12D3KooWPUArBJny2H3tsXvayd298WN4De9K6RW13eMmVceh5X3s \
 --unsafe-ws-external \
 --unsafe-rpc-external \
 --rpc-cors '*' \
