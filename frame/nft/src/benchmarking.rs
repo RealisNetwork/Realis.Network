650e2b2dbb6fa03b6181f59ef7c8592ece88ccb0
danil <kireevdanil2000@gmail.com>
2021-06-26 17:29:08 +0300

    improve nft benchmarks but still not work properly

--- frame/nft/src/benchmarking.rs
+++ frame/nft/src/benchmarking.rs
@@ -4,23 +4,37 @@ mod benchmarking {
 	use crate::{*};
 	use frame_benchmarking::{benchmarks, impl_benchmark_test_suite};
 	use frame_system::RawOrigin;
+	use hex_literal::hex;
+
+	// Get Alice AccountId
+	fn alice<T: Config>() -> T::AccountId {
+		let bytes = hex!("10f908b91793b30fc4870e255a0e102745e2a8f268814cd28389ba7f4220764d");
+		T::AccountId::decode(&mut &bytes[..]).unwrap_or_default()
+	}
 
 	benchmarks!{
-	mint_benchmarking {
-		// The upper bound can be increased for greater accuracy
-		let token_id: Range<AccountId> = 1 .. 1000;
-	}: mint(
-			RawOrigin::Root,
-			token_id.into(),
-			U256([1, 0, 0, 0]),
-			Rarity::Common,
-			Socket::Head,
-			Params {
-				strength: 1,
-				agility: 1,
-				intelligence: 1
-			}
-		)
+		// TODO fix error: NotNftMaster
+		// Name of function to benchmark
+		mint {
+			// The upper bound can be increased for greater accuracy
+			let caller = alice::<T>();
+			// Can be named only as letter
+			let b in 1 .. 1000;
+		}: _(
+				RawOrigin::Signed(caller.clone()),
+				alice::<T>(),
+				U256([b.into(), 0, 0, 0]),
+				Rarity::Common,
+				Socket::Head,
+				Params {
+					strength: 1,
+					agility: 1,
+					intelligence: 1
+				}
+			)
+		verify {
+			assert_eq!(1, 1);
+		}
 	}
 
 	impl_benchmark_test_suite!(
