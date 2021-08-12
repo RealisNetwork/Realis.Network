b608f23af924696184623719bbd8c9a19d77f91a
Daelon02 <Daelon02@github.com>
2021-08-12 09:40:28 +0200

    Update realis.json

--- docker/run-realis.sh
+++ docker/run-realis.sh
@@ -12,7 +12,6 @@ ${IMAGE} \
 --chain ../realis.json \
 --ws-port 9944 \
 --rpc-port 9933  \
---reserved-nodes /ip4/135.181.18.215/tcp/30333/p2p/12D3KooWK24U97wgj8CDPt91VGkyup8myj6mJp747ZCZhdtj9x1u \
 --validator  \
 --rpc-methods=Unsafe  \
 --name MyNode01 \
