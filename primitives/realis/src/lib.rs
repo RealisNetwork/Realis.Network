c35c39f31cdda31cd6cf34333af286c12dad7d96
HolyGrease <kireevdanil2000@gmail.com>
2021-09-20 17:43:27 +0300

    refactor: serialize/deserialize Rarity to u8

--- primitives/realis/src/lib.rs
+++ primitives/realis/src/lib.rs
@@ -1,13 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_support::pallet_prelude::{Decode, Encode};
+use serde_repr::{Serialize_repr, Deserialize_repr};
+use sp_std::fmt::{Display, Formatter};
 use primitive_types::U256;
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
-use sp_std::fmt::{Display, Formatter};
-use sp_std::vec::Vec;
-
-use sp_std::str::FromStr;
 
 pub type TokenId = U256;
 pub type Basic = u8;
@@ -40,40 +38,15 @@ pub enum Stackable {
 }
 
 #[derive(Encode, Decode, Clone, Eq, PartialEq, PartialOrd, Ord, Debug, Copy)]
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "std", derive(Serialize_repr, Deserialize_repr))]
+#[repr(u8)]
 pub enum Rarity {
-    Common,
-    Uncommon,
-    Rare,
-    Epic,
-    Legendary,
-    Relic
-}
-
-#[derive(PartialEq, Eq, Clone, Default, Encode, Decode)]
-pub struct OpaqueExtrinsic(Vec<u8>);
-
-impl OpaqueExtrinsic {
-    /// Convert an encoded extrinsic to an `OpaqueExtrinsic`.
-    pub fn from_bytes(mut bytes: &[u8]) -> Result<Self, codec::Error> {
-        OpaqueExtrinsic::decode(&mut bytes)
-    }
-}
-
-impl FromStr for Rarity {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "Common" => Ok(Rarity::Common),
-            "Uncommon" => Ok(Rarity::Uncommon),
-            "Rare" => Ok(Rarity::Rare),
-            "Mythical" => Ok(Rarity::Epic),
-            "Legendary" => Ok(Rarity::Legendary),
-            "Relic" => Ok(Rarity::Relic),
-            _ => Err(()),
-        }
-    }
+    Common = 1,
+    Uncommon = 2,
+    Rare = 3,
+    Epic = 4,
+    Legendary = 5,
+    Relic = 6,
 }
 
 impl Display for Rarity {
