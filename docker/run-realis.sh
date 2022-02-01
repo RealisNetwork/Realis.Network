fc9e839ebec14f793d5db8dfb1dc64b5d7eb3330
Daelon02 <gubin0202@gmail.com>
2022-02-01 15:12:00 +0100

    Update realis.json

--- docker/run-realis.sh
+++ docker/run-realis.sh
@@ -13,8 +13,8 @@ ${IMAGE} \
 --ws-port 9944 \
 --rpc-port 9933  \
 --validator  \
---reserved-nodes /ip4/135.181.18.215/tcp/30333/p2p/12D3KooW9poizzemF6kb6iSbkoJynMhswa4oJe5W9v34eFuRcU47 \
---rpc-methods=Unsafe  \
+--reserved-nodes /ip4/65.108.106.28/tcp/30333/p2p/12D3KooWKbNmi8QsTwr1pWn16dQojpEzUYXztjWkAUHm9ge9BLMD \
+--rpc-methods=Unsafe \
 --name MyNode01 \
 --unsafe-ws-external \
 --unsafe-rpc-external \
