207652f9e8d8b4119b6adf4cb6e5ce965d5e2593
Your Name <you@example.com>
2022-03-31 10:21:31 +0300

    update

--- scripts/run_relay_chain_bob.sh
+++ scripts/run_relay_chain_bob.sh
@@ -3,6 +3,6 @@
   --validator \
   --base-path /tmp/relay/bob \
   --chain chain-spec.json \
-  --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWH1qksqm6iRE796mYPuaWKnZoLwMSRMhhdMk8penzpArF \
+  --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWMCKpKQFDZ6Dq5GCm3z1TMuW6NbNFrxd4edqAgUASz6mf \
   --port 30334 \
   --ws-port 9945
--- scripts/run_relay_chain_charlie.sh
+++ scripts/run_relay_chain_charlie.sh
@@ -3,6 +3,6 @@
   --validator \
   --base-path /tmp/relay/charlie \
   --chain chain-spec.json \
-  --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWFcuUc2VZVvbfVuWTWXMWodLjrgH4FKXveJcRvW5GCYmz \
+  --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWMCKpKQFDZ6Dq5GCm3z1TMuW6NbNFrxd4edqAgUASz6mf \
   --port 30335 \
   --ws-port 9946
