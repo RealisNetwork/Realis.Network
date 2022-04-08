ea55f2e2241bb1ccf458402fed25aead3c4d24e1
BeginArtem <lartem.1205@gmail.com>
2022-04-08 19:17:30 +0300

    change: change spec_name

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -108,7 +108,7 @@ pub fn wasm_binary_unwrap() -> &'static [u8] {
 /// Runtime version.
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("node"),
+    spec_name: create_runtime_str!("realis"),
     impl_name: create_runtime_str!("realis-node"),
     authoring_version: 10,
     // Per convention: if the runtime behavior changes, increment spec_version
